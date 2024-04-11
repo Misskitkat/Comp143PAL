fd4e4d1b02fa878910c2402c33b6b1e6dbed4d5f
Misskitkat <kboscombe@student.bridgew.edu>
2024-04-11 09:18:04 -0400

    edited functions file

--- functions.py
+++ functions.py
@@ -17,5 +17,12 @@ def input_function():
 def add_num(num1,num2):
     print(num1+num2)
 
-add_num(5,7)
+#add_num(5,7)
 
+taskList = [[1,"make bed", "not completed"], [2,"eat breakfast", "completed"]]
+taskList[0][2] = "completed"
+user_input = input("Task?: ")
+taskList.append([1,user_input,"not completed"])
+
+for i in taskList:
+    print(i[0], i[1], i[2])
